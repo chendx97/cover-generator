1b15f892a0a69c778bbbfa4738cbbf4632649429
chendx97 <3310115926@qq.com>
2024-10-11 22:53:55 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/your repository/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/cover-generator/' : '/',
   plugins: [vue()],
 })
